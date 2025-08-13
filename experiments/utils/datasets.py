bcfc2228b03081efe340b481010113d27ac2a4b6
rychoy <riechan.ye@outlook.com>
2025-08-13 14:24:25 +0000

    Modify the input parameters of ParquetImageDataset to be the directory name instead of the file list.

--- experiments/utils/datasets.py
+++ experiments/utils/datasets.py
@@ -1,5 +1,6 @@
 import pyarrow.parquet as pq
 import io
+import os
 
 
 from PIL import Image
@@ -8,10 +9,12 @@ from typing import Optional, Callable
 
 
 class ParquetImageDataset(Dataset):
-    def __init__(self, parquet_files: list, transform: Optional[Callable] = None):
+    def __init__(self, parquet_dir: str, transform: Optional[Callable] = None):
+        assert os.path.exists(parquet_dir), f"Parquet directory {parquet_dir} does not exist"
+
         self.samples = []
-        for file in parquet_files:
-            table = pq.read_table(file).flatten()
+        for fname in os.listdir(parquet_dir):
+            table = pq.read_table(os.path.join(parquet_dir, fname)).flatten()
             columns = table.to_pydict()
             self.samples.extend(zip(columns['image.bytes'], columns['label']))
         self.transform = transform
@@ -25,3 +28,25 @@ class ParquetImageDataset(Dataset):
         if self.transform:
             image = self.transform(image)
         return image, label
+    
+
+if __name__ == '__main__':
+    from timm.data.dataset import ImageDataset
+    from torchvision import transforms
+    from torch.utils.data import DataLoader
+
+    transform = transforms.Compose([
+        transforms.Resize(224),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+    ])
+
+    d = ImageDataset(root="/nfs5/yrc/dataset/imagenet-1k/ILSVRC2012_img_train.tar")
+    
+    print(len(d))
+    
+    l = DataLoader(d, batch_size=128, shuffle=True, num_workers=4)
+    for img, label in l:
+        print(img.shape, label)
+        break
+    
