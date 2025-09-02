933995b057706c9d06d47709d0501d1a678732a3
rychoy <riechan.ye@outlook.com>
2025-09-02 16:05:13 +0000

    Separate experiments.

--- experiments/train.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# from scratch
-OMP_NUM_THREADS=1 torchrun --nproc-per-node=3 src/train_vit.py
-
-# from ckpt
-# OMP_NUM_THREADS=1 torchrun --nproc-per-node=3 src/train_vit.py --resume-path ./checkpoints/my-vit-tiny-patch16-224/20250807-152950-vit_tiny_patch16_224/checkpoint-63.pth.tar
--- experiments/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from .calculate import (
-    calculate_inference_time,
-    calculate_flops
-)
-
-from .plot import (
-    plot_performance
-)
